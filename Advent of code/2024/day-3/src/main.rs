63a4c6f34b49dab955655562fdc17ca953b29e33
OleBoleSkole3 <43952813+oleboleskole3@users.noreply.github.com>
2025-01-13 09:46:27 +0100

    aoc day 3 part 2

--- Advent of code/2024/day-3/src/main.rs
+++ Advent of code/2024/day-3/src/main.rs
@@ -4,19 +4,40 @@ use regex::Regex;
 const DATAPATH: &str = "data.txt";
 
 fn main() {
-    let re = Regex::new(r"mul\((?<arg1>\d*),(?<arg2>\d*)\)").unwrap();
+    let re = Regex::new(r"(?<instr>(mul\((?<arg1>\d*),(?<arg2>\d*)\))|(do\(\))|(don't\(\)))").unwrap();
 
     let data = read_data();
 
     let mut sum_all = 0;
+    let mut sum_enabled = 0;
+    let mut enabled = true;
 
     for caps in re.captures_iter(data.as_str()) {
-        println!("Arg 1 {}, Arg 2 {}", &caps["arg1"], &caps["arg2"]);
+        match &caps["instr"] {
+            "do()" => {
+                enabled = true;
+                continue;
+            }
+            "don't()" => {
+                enabled = false;
+                continue;
+            }
+            _ => {}
+        }
+        // println!("Arg 1 {}, Arg 2 {}", &caps["arg1"], &caps["arg2"]);
 
-        sum_all += caps["arg1"].parse::<i32>().unwrap() * caps["arg2"].parse::<i32>().unwrap();
+        let arg1 = caps["arg1"].parse::<i32>().unwrap();
+        let arg2 = caps["arg2"].parse::<i32>().unwrap();
+
+        sum_all += arg1 * arg2;
+
+        if enabled {
+            sum_enabled += arg1 * arg2;
+        }
     }
 
     println!("Sum of multiplications: {sum_all}");
+    println!("Sum of enabled multiplications: {sum_enabled}");
 }
 
 fn read_data() -> String {
