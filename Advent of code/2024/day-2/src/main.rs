f67d7ec60884247c738ebefdbe73f129a57517aa
OleBoleSkole3 <43952813+oleboleskole3@users.noreply.github.com>
2025-01-18 23:56:36 +0100

    partially complete aoc day 2 part 2

--- Advent of code/2024/day-2/src/main.rs
+++ Advent of code/2024/day-2/src/main.rs
@@ -11,8 +11,90 @@ const DATAPATH: &str = "data.txt";
 
 fn main() {
     let data = read_data();
-    let reports = data.split('\n');
+    let reports:std::str::Split<'_, char> = data.split('\n');
 
+    part_one(reports);
+    let reports:std::str::Split<'_, char> = data.split('\n');
+    part_two(reports);
+}
+
+fn part_two(reports: std::str::Split<'_, char>) {
+    let mut safe_reports = 0;
+    for report in reports {
+        let mut dir = ReportDir::Unknown;
+        let mut previous = None;
+
+        let mut fail = false;
+        let mut skipped = false;
+        report.trim().split(' ').enumerate().for_each(|(index, val)| {
+            let val: i32 = val.parse().expect("{val} not number");
+
+            if let Some(prev) = previous {
+                let diff: i32 = val - prev;
+                if (diff).abs() == 0 || diff.abs() > 3 {
+                    if skipped {
+                        fail = true;
+                        print!("fail, ")
+                    } else {
+                        if index == 1 {
+                            previous = Some(val);
+                        }
+                        skipped = true;
+                        return;
+                    }
+                } 
+                match dir {
+                    ReportDir::Rising => {
+                        if diff < 0 {
+                            if skipped {
+                                fail = true;
+                                print!("fail, ")
+                            } else {
+                                if index == 1 {
+                                    previous = Some(val);
+                                }
+                                skipped = true;
+                                return;
+                            }
+                        }
+                    }
+                    ReportDir::Falling => {
+                        if diff > 0 {
+                            if skipped {
+                                fail = true;
+                                print!("fail, ")
+                            } else {
+                                if index == 1 {
+                                    previous = Some(val);
+                                }
+                                skipped = true;
+                                return;
+                            }
+                        }
+                    }
+                    ReportDir::Unknown => {
+                        if diff > 0 {
+                            dir = ReportDir::Rising;
+                        } else {
+                            dir = ReportDir::Falling;
+                        }
+                    }
+                }
+                print!("{diff}, {fail}, ")
+            }
+            println!();
+
+            previous = Some(val);
+        });
+        if !fail {
+            safe_reports += 1;
+        }
+    }
+
+    println!("Safe reports (with skips): {safe_reports}");
+}
+
+fn part_one(reports: std::str::Split<'_, char>) {
     let mut safe_reports = 0;
     for report in reports {
         let mut dir = ReportDir::Unknown;
